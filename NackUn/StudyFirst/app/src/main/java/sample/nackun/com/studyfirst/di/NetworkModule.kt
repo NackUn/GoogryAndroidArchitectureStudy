80dc6eff7b9e53f2b33348f2a01216bb91a5f0fc
NackUn <king4521@naver.com>
2020-01-11 15:32:23 +0900

    ETC-2 di 추가 및 변경

--- NackUn/StudyFirst/app/src/main/java/sample/nackun/com/studyfirst/di/NetworkModule.kt
+++ NackUn/StudyFirst/app/src/main/java/sample/nackun/com/studyfirst/di/NetworkModule.kt
@@ -6,16 +6,14 @@ import retrofit2.Converter
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
-import sample.nackun.com.studyfirst.network.UpbitApi
 
-fun getNetworkModule(baseUrl: String) = module {
-    single {
+val networkModule = module {
+    factory { (baseUrl: String) ->
         Retrofit.Builder()
             .baseUrl(baseUrl)
             .addConverterFactory(get())
             .addCallAdapterFactory(get())
             .build()
-            .create(UpbitApi::class.java)
     }
 
     single {
--- NackUn/StudyFirst/app/src/main/java/sample/nackun/com/studyfirst/di/RemoteModule.kt
+++ NackUn/StudyFirst/app/src/main/java/sample/nackun/com/studyfirst/di/RemoteModule.kt
@@ -1,9 +1,18 @@
 package sample.nackun.com.studyfirst.di
 
+import org.koin.core.parameter.parametersOf
 import org.koin.dsl.module
-import sample.nackun.com.studyfirst.data.DataSource
-import sample.nackun.com.studyfirst.data.remote.RemoteDataSource
+import retrofit2.Retrofit
+import sample.nackun.com.studyfirst.data.bithumb.BithumbDataSource
+import sample.nackun.com.studyfirst.data.bithumb.remote.BithumbRemoteDataSource
+import sample.nackun.com.studyfirst.data.upbit.UpbitDataSource
+import sample.nackun.com.studyfirst.data.upbit.remote.UpbitRemoteDataSource
+import sample.nackun.com.studyfirst.network.BithumbApi
+import sample.nackun.com.studyfirst.network.UpbitApi
 
 val remoteModule = module {
-    single<DataSource> { RemoteDataSource(get()) }
+    single<UpbitApi> { get<Retrofit> { parametersOf("https://api.upbit.com") }.create(UpbitApi::class.java) }
+    single<BithumbApi> { get<Retrofit> { parametersOf("https://api.bithumb.com") }.create(BithumbApi::class.java) }
+    single<UpbitDataSource> { UpbitRemoteDataSource(get()) }
+    single<BithumbDataSource> { BithumbRemoteDataSource(get()) }
 }
