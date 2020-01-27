adff76964cf1db7aead70cc49fc293b10f179689
NackUn <king4521@naver.com>
2020-01-27 14:47:28 +0900

    ETC-3 UseCaseModule 추가

---
+++ NackUn/StudyFirst/app/src/main/java/sample/nackun/com/studyfirst/di/UseCaseModule.kt
@@ -0,0 +1,20 @@
+package sample.nackun.com.studyfirst.di
+
+import org.koin.dsl.module
+import sample.nackun.com.studyfirst.domain.GetBithumbTickersUseCase
+import sample.nackun.com.studyfirst.domain.GetUpbitMarketUseCase
+import sample.nackun.com.studyfirst.domain.GetUpbitTickersUseCase
+
+val useCaseModule = module {
+    single {
+        GetUpbitMarketUseCase(get())
+    }
+
+    single {
+        GetUpbitTickersUseCase(get())
+    }
+
+    single {
+        GetBithumbTickersUseCase(get())
+    }
+}
