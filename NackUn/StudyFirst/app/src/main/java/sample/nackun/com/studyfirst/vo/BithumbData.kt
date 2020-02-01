0aa52fefe803b2437771f8c89d5192846612ba3a
NackUn <king4521@naver.com>
2020-02-01 18:38:31 +0900

    ETC-4 사용하지않는 BithumbData 삭제

--- NackUn/StudyFirst/app/src/main/java/sample/nackun/com/studyfirst/vo/BithumbData.kt
+++
@@ -1,9 +0,0 @@
-package sample.nackun.com.studyfirst.vo
-
-
-import com.google.gson.annotations.SerializedName
-
-data class BithumbData(
-    @SerializedName("BithumbTicker")
-    val bithumbTickers: List<BithumbTicker>
-)
